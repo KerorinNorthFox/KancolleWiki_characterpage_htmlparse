2dfdaf19a654fa56bf8ac320317b31d03288f3f0
kerorin kitakitsune <66837061+KerorinNorthFox@users.noreply.github.com>
2021-07-08 15:25:27 +0900

    Create htmlparse.py

---
+++ htmlparse.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests, time, csv
+
+while(True)
+  
+
+
+print(torpedo[0].contents[0])
+print(aa[0].contents[0])
+print(asw[0].contents[0])
+print(los[0].contents[0])
+print(luck[0].contents[0])
+print(ammo[0].contents[0])
